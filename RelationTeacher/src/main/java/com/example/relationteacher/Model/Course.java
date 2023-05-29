3d70ef59c985b1def7ee5a94b56f93e99c36d78f
Amwaj Alotaibi <132268852+Amwajalotaibi@users.noreply.github.com>
2023-05-29 14:34:38 +0300

    Add files via upload

---
+++ RelationTeacher/src/main/java/com/example/relationteacher/Model/Course.java
@@ -0,0 +1,27 @@
+package com.example.relationteacher.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "varchar(10) not null")
+    private String name;
+
+
+    @ManyToOne
+    @JoinColumn(name = "teacher" ,referencedColumnName = "id")
+    @JsonIgnore
+    private Teacher teacher;
+}
